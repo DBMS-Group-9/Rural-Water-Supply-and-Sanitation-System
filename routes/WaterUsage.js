38c857e5cf69109fe48c46f4953a64fdfe7eb9c9
Venkat <venkatasubramanian2001@gmail.com>
2020-11-11 11:23:12 +0530

    water usage route updated

--- routes/WaterUsage.js
+++ routes/WaterUsage.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const connection = require('./../db');
 const config = require('./../config');
 
+router.get('/getallwaterusage', (req, res, next) => {
+    connection.query(`SELECT * from WaterUsage`, function (err, result) {
+        if(err) 
+        {
+            res.status(500).json({ message: err.toString() });
+            return;
+        }
+        res.status(200).json({ message: "Water Usage Fetched Successfully!", result });
+    });
+});
+
 router.post('/addwaterusage', (req, res, next) => {
-    connection.query(`INSERT into WaterSources(Month,Year,Usages) values(${req.body.Month},${req.body.Year},${req.body.Usages})`, function (err, result) {
+    connection.query(`INSERT into WaterUsage(WSID,Month,Year,Usages) values(${req.body.WSID},${req.body.Month},${req.body.Year},${req.body.Usages})`, function (err, result) {
         if(err) {
             res.status(500).json({ message: err.toString() });
             return;
@@ -14,5 +25,3 @@ router.post('/addwaterusage', (req, res, next) => {
 });
 
 module.exports = router;
-
-//How are we adding the usage? and should WSID be added?
